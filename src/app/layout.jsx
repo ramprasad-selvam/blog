e455c1f12658eb24ed4289285e48e9beaabbc054
Ramprasad <ramprasad1@justdial.com>
2025-09-18 12:50:45 +0530

    check analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 import { Roboto } from "next/font/google"
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className={roboto.className}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
