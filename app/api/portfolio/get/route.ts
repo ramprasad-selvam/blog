720fe6a48bfc9dc50766b7031ce376c609b5d643
Ramprasad <ramprasadselvam@gmail.com>
2026-01-11 16:24:38 +0530

    download

--- app/api/portfolio/get/route.ts
+++
@@ -1,9 +0,0 @@
-import { Redis } from "@upstash/redis";
-import { NextResponse } from "next/server";
-
-const redis = Redis.fromEnv();
-
-export async function GET() {
-  const data = await redis.get("portfolio_data");
-  return NextResponse.json(data);
-}
