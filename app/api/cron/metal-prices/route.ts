62803b973e09fe0d16b2825d97461a568296809a
Ramprasad <ramprasad1@justdial.com>
2026-05-06 16:43:18 +0530

    delete older data than 90 days

--- app/api/cron/metal-prices/route.ts
+++ app/api/cron/metal-prices/route.ts
@@ -21,11 +21,26 @@ export async function GET(request: Request) {
     const result = await sql`
       INSERT INTO precious_metals (date, gold_24k, gold_22k, gold_18k, gold_14k, platinum, silver)
       VALUES (CURRENT_DATE, ${rates.gold_24k}, ${rates.gold_22k}, ${rates.gold_18k}, ${rates.gold_14k}, ${rates.platinum}, ${rates.silver})
-      ON CONFLICT (date) DO UPDATE SET gold_24k = EXCLUDED.gold_24k
+      ON CONFLICT (date) DO UPDATE SET 
+        gold_24k = EXCLUDED.gold_24k,
+        gold_22k = EXCLUDED.gold_22k,
+        gold_18k = EXCLUDED.gold_18k,
+        gold_14k = EXCLUDED.gold_14k,
+        platinum = EXCLUDED.platinum,
+        silver = EXCLUDED.silver
       RETURNING *;
     `;
 
     console.log("DB Result:", result.rows);
+
+    await sql`
+      DELETE FROM precious_metals
+      WHERE date NOT IN (
+        SELECT date FROM precious_metals
+        ORDER BY date DESC
+        LIMIT 90
+      );
+    `;
     return NextResponse.json({ success: true, data: result.rows });
 
   } catch (error) {
